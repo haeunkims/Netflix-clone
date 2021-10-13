af929a7bdb1bf8183ff4de0175b9ce5f7bac56c0
Haeun Kim <gracekim.an@gmail.com>
2021-10-13 16:17:31 +0900

    get all movies using api

---
+++ src/request.js
@@ -0,0 +1,13 @@
+const API_KEY = "a5990b8e70dcebd983e2b8786f0cf2a0";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_keys=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
