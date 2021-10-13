6681a495e4b13eb24170ec6a151787f07055e331
Haeun Kim <gracekim.an@gmail.com>
2021-10-13 19:23:48 +0900

    Styling buttons and desc

--- src/Banner.js
+++ src/Banner.js
@@ -20,6 +20,10 @@ function Banner() {
   }, []);
   console.log(movie);
 
+  function truncates(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -30,13 +34,17 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
 
         <div className="banner_buttons">
           <button className="banner_button">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 className="banner_description">
+          {truncates(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   );
