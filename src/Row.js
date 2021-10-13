045722cb61804438ad8f7826dc1d88f8249cd456
Haeun Kim <gracekim.an@gmail.com>
2021-10-13 18:55:08 +0900

    Adding banner

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // a snippet of code which runs based on specific conditions
@@ -17,7 +17,6 @@ function Row({ title, fetchUrl }) {
     fetchData();
     // if [], run once when the row loads, and don't run again, load page.
   }, [fetchUrl]);
-  console.table(movies);
 
   return (
     <div className="row">
@@ -26,8 +25,11 @@ function Row({ title, fetchUrl }) {
         {/* component --> posters */}
         {movies.map((movie) => (
           <img
-            className="row_poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
