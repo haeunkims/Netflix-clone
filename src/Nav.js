b3d7402769d1ab6b95581d24592d935d895ebdcb
Haeun Kim <gracekim.an@gmail.com>
2021-10-14 16:05:14 +0900

    adding video trailers

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
-  return <div className="nav"></div>;
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2014_logo.svg/640px-Netflix_2014_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="User Avatar"
+      />
+    </div>
+  );
 }
 
 export default Nav;
